15282980c2877ff7cac2378c1a186d547af7d670
Lluc <lluc.serramitjana@estudiantat.upc.edu>
2021-12-13 13:10:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mybank/MainActivity.java
@@ -0,0 +1,171 @@
+package com.example.mybank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirst.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new HelloMessage(this).execute("http://10.0.2.2:9000/Application/InicialitzarBaseDades" );
+    }
+    EditText cantant;
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText editText = (EditText) findViewById(R.id.EditResult);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void Login(View view) {
+
+
+        new Thread(new Runnable() {
+            InputStream stream = null;
+            String str = "";
+            String result = null;
+            Handler handler = new Handler();
+
+            public void run() {
+
+                try {
+
+                    cantant =findViewById(R.id.EditQuery);
+                    String query = String.format("http://10.0.2.2:9000/Application/NombreDiscs?nom="+cantant.getText().toString());
+                    URL url = new URL(query);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(10000);
+                    conn.setConnectTimeout(15000 /* milliseconds */);
+                    conn.setRequestMethod("GET");
+                    // conn.setRequestMethod("POST");
+                    conn.setDoInput(true);
+                    conn.setDoOutput(true);
+                    conn.connect();
+
+                   /* String params = "fullname="+ R.id.etUsername +"&Password=" + R.id.etPassword;
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(params.toString());
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                      */
+
+                    stream = conn.getInputStream();
+
+                    BufferedReader reader = null;
+
+                    StringBuilder sb = new StringBuilder();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    result = sb.toString();
+
+                    // Mostrar resultat en el quadre de text.
+                    // Codi incorrecte
+                    // EditText n = (EditText) findViewById (R.id.edit_message);
+                    //n.setText(result);
+
+                    //Codi correcte
+                    Log.i("MainActivity", result);
+                    handler.post(new Runnable() {
+                        public void run() {
+                            EditText n = (EditText) findViewById(R.id.EditResult);
+                            n.setText(result);
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+    
+
+
+
+    private class HelloMessage extends AsyncTask<String, Void, String> {
+        Context context;
+        InputStream stream = null;
+        String str = "";
+        String result = null;
+
+        private HelloMessage(Context context) {
+            this.context = context;
+        }
+        @Override
+        protected String doInBackground(String... urls) {
+
+            try {
+                String query = String.format(urls[0]);
+                URL url = new URL(query);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+                stream = conn.getInputStream();
+
+                BufferedReader reader = null;
+
+                StringBuilder sb = new StringBuilder();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                result = sb.toString();
+
+
+                Log.i("lolaforms1", result);
+
+
+                return result;
+
+            } catch(IOException e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            EditText n = (EditText) findViewById (R.id.EditResult);
+            n.setText(result);
+
+        }
+    }
+
+}
